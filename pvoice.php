770e8169e89d1711ac9100bb4db80b4dbaf1341f
Md. Aamir Sohail <42406623+Aamir21@users.noreply.github.com>
2021-03-16 21:43:30 +0530

    Add files via upload

---
+++ pvoice.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+        
+    
+    
+    
+    
+     <head>
+         
+         
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+       
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+        
+         
+         
+         
+         <meta charset="utg-8">
+    
+        <meta name="viewport" width="device-width , initial-scale=1, shrink-to-fit=no">
+            
+            
+            
+            <style type="text/css">
+                
+                .segment1{
+                    height: 90px;
+                    background-color: darkslategray;
+                    display: flex;
+                    justify-content: flex-end;
+                    box-shadow: 0px 0px 3px grey;
+                    
+                }
+                
+                
+                .logo{
+                    color: aliceblue;
+                    font-family: League Spartan;
+                    font-size: 35px;
+                    display: flex;
+                    justify-content: flex-start;
+                    padding-top: 30px;
+                    
+                    
+                }
+                
+                .plogo{
+                    display: flex;
+                    text-shadow: 0px 0px 2px aliceblue;
+                    transition: all .7s linear;
+                }
+                .plogo:hover{
+                    text-shadow: 0px 0px 4px aliceblue;
+                    transition: all .7s linear;
+                    
+                }
+                
+                
+                .segment1_1{
+                    height: 25px;
+                    background-color: transparent;
+                    color: lightgreen;
+                    padding-top: 2px;
+                    margin-top: 64px;
+                    float: left;
+                    display: flex;
+                    justify-content: center;
+                    margin-left: 5px;
+                    font-family: Poppins;
+                     transition: all 0.7s ease-out;
+                    
+                }
+                .segment1_1:hover{
+                    cursor: pointer;
+                    background-color: lightgreen;
+                    color: darkslategrey;
+                    transition: all 0.7s ease-out;
+                    
+                }
+                
+            
+                .content1{
+                    height: 500px;
+                    background-color: whitesmoke;
+                }
+                
+                 .content2{
+                    height: 500px;
+                    background-color: gainsboro;
+                     
+                }
+                
+                .content2_1{
+                    
+                    height: 150px;
+                    background-color: cadetblue;
+                    margin-top: 10px;
+                    display: flex;
+                    border: solid thin grey;
+                    border-radius: 4px;
+                    box-shadow: 0px 0px 3px gray;
+                    
+                    
+                }
+                
+                .content3{
+                    
+                    height: 250px;
+                    background-color:darkgray;
+                }
+                
+                .content3_1{
+                    height: 200px;
+                    background-color: chocolate;
+                    margin-top: 5px;
+                    margin-left: 5px;
+                    float: left;
+                    padding: 0px 0px 0px 0px;
+                    justify-content: center;
+                    display: inline-flex;
+                }
+                
+                .footer{
+                    height: 200px;
+                    background-color: green;
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                /*media query*/
+                
+                @media screen and (max-width: 360px){
+                    
+                    .content1{
+                        height: 150px;
+                        display: none;
+                    }
+                    
+                    .content2{
+                        
+                        height: 100px;
+                        
+                    }
+                    
+                    .content3{
+                        
+                    }
+                    
+                    .content3_1{
+                        width: 30px;
+                        height: 30px;
+                        
+                    }
+                    
+                    .logo{
+                       display: sticky;
+                       
+                        
+                    
+                        
+                        
+                    }
+                    
+                    .footer{
+                        display: flex;
+                    }
+                    
+                    
+                }
+             
+                
+                
+                
+                
+                
+                
+            
+            </style>
+        
+        
+        
+    </head>
+        
+
+    <body>
+        
+         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Advent+Pro|Alfa+Slab+One|Anton|Archivo+Narrow|Baloo+2|Baloo+Chettan+2|Bellota|BenchNine|Caveat|Cinzel|Comfortaa|Concert+One|Cookie|Damion|Dancing+Script|Dosis|Electrolize|Fredoka+One|Great+Vibes|Indie+Flower|Inter|Josefin+Sans|Josefin+Slab|Kaushan+Script|Lato|Lobster|Luckiest+Guy|Monoton|Montserrat|Open+Sans|Oswald|Oxanium|PT+Sans+Caption|Pacifico|Parisienne|Passion+One|Poiret+One|Poppins|Quattrocento+Sans|Raleway|Righteous|Roboto+Mono|Sacramento|Satisfy|Sen|Source+Sans+Pro|Spartan|Titillium+Web|Ubuntu|Ultra|Yellowtail&display=swap" rel="stylesheet">
+        
+        
+        
+        
+       
+        
+        <div class="container-fluid">
+        
+            <div class="row">                                                   <!--ROW1-->
+                
+                <div class="col-md-12 segment1">
+                    
+                   
+                    
+                    <div class="col-sm-8 logo"><p class="plogo">Trender.IN</p></div>
+                    
+            
+                
+                    <div class="col-md-1 segment1_1">Home</div>
+                    <div class="col-md-1 segment1_1">Live</div>
+                    <div class="col-md-1 segment1_1">Weather</div>
+                    <div class="col-md-1 segment1_1">Stock</div>
+                
+                </div>
+                
+                
+            
+            
+            </div>
+            
+            <div class="row">                                                   <!---ROW3-->
+            
+                <div class="col-md-7 content1"></div>
+                
+                <div class="col-md-5 content2">
+                
+                     <div class="col-sm-12 content2_1"></div>
+                    <div class="col-sm-12 content2_1"></div>
+                    <div class="col-sm-12 content2_1"></div>
+                
+                </div>
+               
+                
+                
+            </div>
+            
+            <div class="row">                                                   <!--ROW3-->
+            
+                <div class="col-md-12 content3">
+                
+                    <div class="col-lg-2 content3_1"></div>
+                    <div class="col-lg-3 content3_1"></div>
+                    <div class="col-lg-3 content3_1"></div>
+                    <div class="col-lg-2 content3_1"></div>
+                    
+                
+                </div>
+            
+            </div>
+            
+            <div class="row">
+                
+            
+            
+            </div>
+            
+              
+            
+            
+        
+        </div>
+        
+        
+    
+    
+    
+    </body>
+
+
+
+</html>
